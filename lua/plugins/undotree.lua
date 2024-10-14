ec0b61479f7aab803aef9ae5ba6556c004f5467b
andreyfesunov <fsnv04@gmail.com>
2024-10-14 13:06:23 +0300

    windows undotree diff command

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,5 +2,6 @@ return {
 	"mbbill/undotree",
 	config = function()
 		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree toggle" })
+		vim.g.undotree_DiffCommand = "FC"
 	end,
 }
