33ffdf8ed43efe07dadb1c9eecb61cfb0ec442e7
andreyfesunov <fsnv04@gmail.com>
2024-10-23 15:22:15 +0300

    config colorizer

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,6 +1,22 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	config = function()
-		require("colorizer").setup()
+		require("colorizer").setup({
+			"css",
+			"scss",
+			"json",
+			"html",
+			"js",
+			"ts",
+		}, {
+			RGB = true,
+			RRGGBB = true,
+			names = true,
+			RRGGBBAA = true,
+			rgb_fn = true,
+			hsl_fn = true,
+			css = true,
+			css_fn = true,
+		})
 	end,
 }
