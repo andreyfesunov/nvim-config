ea8e4e42b6940dbe11ed79bf64ee67c7288d8b5c
andreyfesunov <fsnv04@gmail.com>
2024-11-08 20:48:41 +0300

    feature: neotree -> oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,24 @@
+return {
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			lsp_file_methods = {
+				timeout_ms = 15000,
+			},
+			keymaps = {
+				["<C-c>"] = "actions.close",
+				["<C-p>"] = "actions.preview",
+				["<CR>"] = "actions.select",
+				["_"] = "actions.open_cwd",
+				["-"] = "actions.parent",
+				["gs"] = "actions.change_sort",
+				["g?"] = "actions.show_help",
+				["C-r"] = "actions.refresh",
+			},
+			use_default_keymaps = false,
+		})
+
+		vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", { desc = "Open File explorer" })
+	end,
+}
