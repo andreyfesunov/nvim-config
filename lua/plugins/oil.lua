f325724f9766c1289c4c2d915901e64bab75b773
andreyfesunov <fsnv04@gmail.com>
2024-11-09 14:38:04 +0300

    feature: make hidden options visible in oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,24 +1,27 @@
 return {
-	"stevearc/oil.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({
-			lsp_file_methods = {
-				timeout_ms = 15000,
-			},
-			keymaps = {
-				["<C-c>"] = "actions.close",
-				["<C-p>"] = "actions.preview",
-				["<CR>"] = "actions.select",
-				["_"] = "actions.open_cwd",
-				["-"] = "actions.parent",
-				["gs"] = "actions.change_sort",
-				["g?"] = "actions.show_help",
-				["C-r"] = "actions.refresh",
-			},
-			use_default_keymaps = false,
-		})
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+            lsp_file_methods = {
+                timeout_ms = 15000,
+            },
+            keymaps = {
+                ["<C-c>"] = "actions.close",
+                ["<C-p>"] = "actions.preview",
+                ["<CR>"] = "actions.select",
+                ["_"] = "actions.open_cwd",
+                ["-"] = "actions.parent",
+                ["gs"] = "actions.change_sort",
+                ["g?"] = "actions.show_help",
+                ["C-r"] = "actions.refresh",
+            },
+            use_default_keymaps = false,
+            view_options = {
+                show_hidden = true
+            }
+        })
 
-		vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", { desc = "Open File explorer" })
-	end,
+        vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", { desc = "Open File explorer" })
+    end,
 }
