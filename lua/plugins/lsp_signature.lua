6ad6e9419b2ab1da11f2d15a78eed3679420e273
andrey_fesunov <fsnv04@gmail.com>
2025-01-04 17:09:12 +0300

    feat: add lsp signature on write

---
+++ lua/plugins/lsp_signature.lua
@@ -0,0 +1,12 @@
+--[[
+    Plugin that adds method signatures on write
+--]]
+
+return {
+	"ray-x/lsp_signature.nvim",
+	event = "VeryLazy",
+	opts = {},
+	config = function(_, opts)
+		require("lsp_signature").setup(opts)
+	end,
+}
