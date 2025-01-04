ef610c2d7ebdb4f721fba9c620954a799418ad01
andrey_fesunov <fsnv04@gmail.com>
2025-01-04 16:59:12 +0300

    feat: add gd/gr signals as virtual text

---
+++ lua/plugins/action_hints.lua
@@ -0,0 +1,19 @@
+--[[
+    Plugin that adds as virtual text:
+
+    ⊛ - go-to-definition is available
+    ↱ - reference list available / number of references
+--]]
+
+return {
+	"roobert/action-hints.nvim",
+	config = function()
+		require("action-hints").setup({
+			template = {
+				definition = { text = " ⊛", color = "#add8e6" },
+				references = { text = " ↱%s", color = "#ff6666" },
+			},
+			use_virtual_text = true,
+		})
+	end,
+}
