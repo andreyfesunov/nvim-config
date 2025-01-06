340a7f7140e7d365f2878ece0888699373f9aa2f
andreyfesunov <fsnv04@gmail.com>
2025-01-07 02:34:58 +0300

    feat: colorscheme to colorscheme switcher

---
+++ lua/plugins/colorscheme_switcher.lua
@@ -0,0 +1,15 @@
+return {
+	"lmantw/themify.nvim",
+	lazy = false,
+	priority = 999,
+	config = function()
+		require("themify").setup({
+			-- Your list of colorschemes.
+			"folke/tokyonight.nvim",
+			"savq/melange-nvim",
+			-- Built-in colorschemes are also supported.
+			-- (Also works with any colorschemes that are installed via other plugin manager, just make sure the colorscheme is loaded before Themify is loaded.)
+			"default",
+		})
+	end,
+}
