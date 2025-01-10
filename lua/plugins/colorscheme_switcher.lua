ffd69996220f27a5db369db6aa6f15832ba622d9
andreyfesunov <fsnv04@gmail.com>
2025-01-10 16:34:57 +0300

    chore: add poimandres colorscheme

--- lua/plugins/colorscheme_switcher.lua
+++ lua/plugins/colorscheme_switcher.lua
@@ -11,6 +11,7 @@ return {
             -- Your list of colorschemes.
             "folke/tokyonight.nvim",
             "savq/melange-nvim",
+            "olivercederborg/poimandres.nvim",
             -- Built-in colorschemes are also supported.
             -- (Also works with any colorschemes that are installed via other plugin manager, just make sure the colorscheme is loaded before Themify is loaded.)
             "default",
