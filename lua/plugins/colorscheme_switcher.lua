e87afbbc8a9a20c1e592754c6ae85b5fb7cb92d4
andreyfesunov <fsnv04@gmail.com>
2025-01-14 15:51:40 +0300

    feat: add github colorscheme

--- lua/plugins/colorscheme_switcher.lua
+++ lua/plugins/colorscheme_switcher.lua
@@ -3,18 +3,19 @@
 --]]
 
 return {
-    "lmantw/themify.nvim",
-    lazy = false,
-    priority = 999,
-    config = function()
-        require("themify").setup({
-            -- Your list of colorschemes.
-            "folke/tokyonight.nvim",
-            "savq/melange-nvim",
-            "olivercederborg/poimandres.nvim",
-            -- Built-in colorschemes are also supported.
-            -- (Also works with any colorschemes that are installed via other plugin manager, just make sure the colorscheme is loaded before Themify is loaded.)
-            "default",
-        })
-    end,
+	"lmantw/themify.nvim",
+	lazy = false,
+	priority = 999,
+	config = function()
+		require("themify").setup({
+			-- Your list of colorschemes.
+			"folke/tokyonight.nvim",
+			"savq/melange-nvim",
+			"olivercederborg/poimandres.nvim",
+			"projekt0n/github-nvim-theme",
+			-- Built-in colorschemes are also supported.
+			-- (Also works with any colorschemes that are installed via other plugin manager, just make sure the colorscheme is loaded before Themify is loaded.)
+			"default",
+		})
+	end,
 }
