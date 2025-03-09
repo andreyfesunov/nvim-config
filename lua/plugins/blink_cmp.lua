f9386830a0fed6d8c0c30b622b6a2ec25f15c89b
andrey_fesunov <fsnv04@gmail.com>
2025-03-09 19:17:37 +0300

    feat: coc -> lsp_setup, coc.cmp -> blink.cmp

---
+++ lua/plugins/blink_cmp.lua
@@ -0,0 +1,107 @@
+return {
+    {
+        "saghen/blink.cmp",
+        -- optional: provides snippets for the snippet source
+        dependencies = "rafamadriz/friendly-snippets",
+
+        -- use a release tag to download pre-built binaries
+        version = "*",
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
+
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
+            -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
+            -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+            --
+            -- All presets have the following mappings:
+            -- C-space: Open menu or open docs if already open
+            -- C-e: Hide menu
+            -- C-k: Toggle signature help
+            --
+            -- See the full "keymap" documentation for information on defining your own keymap.
+            keymap = {
+                preset = "none",
+
+                ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+                ["<C-e>"] = { "hide", "fallback" },
+                ["<CR>"] = { "accept", "fallback" },
+
+                ["<Tab>"] = { "snippet_forward", "fallback" },
+                ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+                ["<Up>"] = { "select_prev", "fallback" },
+                ["<Down>"] = { "select_next", "fallback" },
+                ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+                ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+                ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+            },
+
+            appearance = {
+                -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+                -- Useful for when your theme doesn't support blink.cmp
+                -- Will be removed in a future release
+                use_nvim_cmp_as_default = true,
+                -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = "mono",
+            },
+
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
+
+            -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+            --
+            -- See the fuzzy documentation for more information
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+        },
+        opts_extend = { "sources.default" },
+    },
+    -- LSP servers and clients communicate which features they support through "capabilities".
+    --  By default, Neovim supports a subset of the LSP specification.
+    --  With blink.cmp, Neovim has _more_ capabilities which are communicated to the LSP servers.
+    --  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
+    --
+    -- This can vary by config, but in general for nvim-lspconfig:
+
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { "saghen/blink.cmp" },
+
+        -- example using `opts` for defining servers
+        opts = {
+            servers = {},
+        },
+        config = function(_, opts)
+            local lspconfig = require("lspconfig")
+
+            for server, config in pairs(opts.servers) do
+                -- passing config.capabilities to blink.cmp merges with the capabilities in your
+                -- `opts[server].capabilities, if you've defined it
+                config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+                lspconfig[server].setup(config)
+            end
+        end,
+
+        -- example calling setup directly for each LSP
+        -- config = function()
+        --     local capabilities = require('blink.cmp').get_lsp_capabilities()
+        --     local lspconfig = require('lspconfig')
+        --
+        --     lspconfig['lua_ls'].setup({ capabilities = capabilities })
+        -- end
+    },
+}
