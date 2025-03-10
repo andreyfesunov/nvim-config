d1d3ea4bdcc3c6043bde9660a5ce0b4a978d1061
andreyfesunov <fsnv04@gmail.com>
2025-03-10 17:44:28 +0300

    fix: return autopairs

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,10 @@
+return {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    opts = {
+        -- map_cr = false,
+    },
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+}
